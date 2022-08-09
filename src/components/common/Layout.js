baa3675b50611719c200810beeacfbe64a3a8320
hklim00 <g5bugi82@gmail.com>
2022-08-10 05:00:23 +0900

    department 레이아웃

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -9,6 +9,9 @@ function Layout({ children, name }) {
 
 	return (
 		<section className={`content ${name}`} ref={frame}>
+			<figure>
+				<h1>{name}</h1>
+			</figure>
 			<div className='inner'>{children}</div>
 		</section>
 	);
--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,35 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
+	const [Members, setMembers] = useState([]);
+
+	const path = process.env.PUBLIC_URL;
+
+	useEffect(() => {
+		axios.get(path + '/DB/members.json').then((json) => {
+			setMembers(json.data.members);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Department'}>
-			<div>Department</div>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='inner'>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<div className='txt'>
+								<h2>{member.name}</h2>
+								<p>{member.position}</p>
+							</div>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
